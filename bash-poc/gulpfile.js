2e399e2e9c5c9ecf313662ec502ea6480d448de6
tanayuk <yuuki.tnk@gmail.com>
2016-12-20 16:23:42 +0900

    Initial commit

---
+++ bash-poc/gulpfile.js
@@ -0,0 +1,9 @@
+
+'use strinct';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('./sass/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./css'));
+});
+
